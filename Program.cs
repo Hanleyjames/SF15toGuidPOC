37d7064b0f43c7c38c29933a95ae6172660f7ecf
James Hanley <39565690+Hanleyjames@users.noreply.github.com>
2023-12-14 18:58:45 -0700

    initial and final commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace poc_62_64;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String salesforceId = "50130000000014C";
+        Console.WriteLine("Salesforce Id: " + salesforceId);
+        Guid guid = salesforceIdToGuid(salesforceId);
+        Console.WriteLine("Guid: " + guid);
+        String salesforceId2 = guidToSalesforceId(guid);
+        Console.WriteLine("Salesforce Id: " + salesforceId2);
+       
+    }
+
+    public static Guid salesforceIdToGuid(string salesforceId)
+    {
+        if(salesforceId.Length != 15)
+        {
+            throw new Exception("Salesforce Id must be 15 characters long");
+        }
+        salesforceId+="0";
+        char[] chars = salesforceId.ToCharArray();
+        byte[] bytes = Encoding.GetEncoding("UTF-8").GetBytes(chars);
+        return new Guid(bytes);
+    }
+
+    public static String guidToSalesforceId(Guid guid)
+    {
+        byte[] bytes = guid.ToByteArray();
+        char[] chars = Encoding.GetEncoding("UTF-8").GetChars(bytes);
+        return new String(chars).Substring(0,15);
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Salesforce ID to GUID and GUID to Salesforce ID Conversion
+
+This project is a proof of concept to take 15 digit salesforceId's and convert them to GUID's. We can also take these GUID's and convert them back to the original 15 digit salesforceId.
